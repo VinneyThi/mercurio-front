9a402d1c2140bb8e13028badeb0e675db8e1f91a
Vinney <thivinney@gmail.com>
2022-04-08 15:21:23 -0300

    Add ref to input value

--- src/components/FormInputs/index.tsx
+++ src/components/FormInputs/index.tsx
@@ -1,17 +1,19 @@
-import { HTMLInputTypeAttribute } from 'react'
+import { HTMLInputTypeAttribute, RefObject } from 'react'
 import './styles.scss'
-interface IProps{
-  labelName : string,
-  logoUse : string,
-  type? :HTMLInputTypeAttribute,
+
+interface IProps {
+  labelName: string,
+  logoUse: string,
+  type?: HTMLInputTypeAttribute,
+  inputRef?:RefObject<HTMLInputElement>
 }
-function FormInputs (props:IProps) {
+
+function FormInputs (props: IProps) {
   return (
-    <div className= 'container-input'>
-        <div className='input-log'><img src={props.logoUse} alt="User svg" />
-</div>
-        <label> {props.labelName}</label>
-        <input type={props?.type || 'text'} className='body-forms-input'/>
+    <div className='container-input'>
+      <div className='input-log'><img src={props.logoUse} alt="User svg" /></div>
+      <label> {props.labelName}</label>
+      <input name={props.labelName.toLocaleLowerCase()} type={props?.type || 'text'} className='body-forms-input' ref={props?.inputRef} />
     </div>
 
   )
